4fd286a3d9d3c312169568f7cea87526780f1f2b
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-02-04 16:20:27 +0100

    node event emitter

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
-var counter = require('./count')
+var stuff = require('./stuff')
 
-console.log(counter(['Shaun', 'Ryu', 'Yoshi']));
+console.log(stuff.counter(['Shaun', 'Ryu', 'Yoshi']))
+console.log(stuff.adder(5, 6))
+console.log(stuff.adder(5, stuff.pi))
+
+var events = require('events')
+
+var myEmitter = new events.EventEmitter()
+
+myEmitter.on('someEvent', function(msg) {
+  console.log(msg)
+})
+
+myEmitter.emit('someEvent', 'the event was emitted')
+
+// 
+
+class Person extends events {
+    constructor(name) {
+        super()
+        this.name = name
+    }
+}
+
+const shaun = new Person('Shaun')
+const ryu = new Person('Ryu')
+const yoshi = new Person('Yoshi')
+
+const people = [shaun, ryu, yoshi]
+
+people.forEach((person) => {
+    person.on('speak', (msg) => {
+        console.log(person.name + ' said: ' + msg)
+    })
+})
+
+shaun.emit('speak', 'hey dudes!')
---
+++ stuff.js
@@ -0,0 +1,15 @@
+var counter = function(arr) {
+  return 'There are ' + arr.length + ' elements in this array'
+}
+
+var adder = function(a, b) {
+  return `The sum of the two number is: ${a+b}`
+}
+
+const pi = 3.142
+
+module.exports = {
+  counter,
+  adder,
+  pi
+}
