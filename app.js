764f19da89ca0b48ffda44be985c9ac4b6497658
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-02-05 13:15:39 +0100

    readable streams

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var http = require('http')
+var fs = require('fs')
 
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8')
+
+myReadStream.on('data', (chunk) => {
+  console.log('new chunk received:')
+  console.log(chunk)
+})
+
+/*
 var server = http.createServer((req, res) => {
   console.log('request was made: ' + req.url)
   res.writeHead(200, {'Content-Type': 'text/plain'})
@@ -8,3 +17,4 @@ var server = http.createServer((req, res) => {
 
 server.listen(3000, '127.0.0.1')
 console.log('yo dawgs, now listening to port 3000')
+*/
