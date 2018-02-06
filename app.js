eb893eb7794b7df0c32fc633ba4b79b2d377b3ab
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-02-06 18:12:13 +0100

    middleware and static files

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express')
+var stylus = require('stylus')
 
 var app = express()
 
 app.set('view engine', 'pug')
+
+app.use('/assets', express.static('assets')).use(stylus.middleware({ // map routes to folder
+  src: __dirname + '/',
+  dest: __dirname + '/'
+}))
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -14,6 +20,5 @@ app.get('/profile/:id', (req, res) => {
   var data = { age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing'] }
   res.render('profile', { person: req.params.id, data: data })
 })
-
 
 app.listen(3000)
