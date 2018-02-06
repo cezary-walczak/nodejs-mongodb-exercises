8f14b591a4ccdabd69f323ed3c79412af192f5df
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-02-06 19:09:15 +0100

    handling post requests

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express')
 var stylus = require('stylus')
+var bodyParser = require('body-parser')
 
 var app = express()
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'pug')
 
@@ -16,6 +19,10 @@ app.get('/', (req, res) => {
 app.get('/contact', (req, res) => {
   console.log(req.query)
   res.render('contact', { qs: req.query })
+})
+app.post('/contact', urlencodedParser, (req, res) => {
+  console.log(req.body)
+  res.render('contact-success', { data: req.body })
 })
 app.get('/profile/:id', (req, res) => {
   var data = { age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing'] }
