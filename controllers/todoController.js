6926b3fbfdd448c5973afe8da3e1bf7891e9c8ce
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-02-07 18:04:11 +0100

    adding model and interaction

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,11 +1,23 @@
+var bodyParser = require('body-parser')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+var data = [{item: 'get milk'}, 
+            {item: 'walk dog'}, 
+            {item: 'kick some coding ass'}]
+
 module.exports = function(app) {
   app.get('/todo', (req, res) => {
-    res.render('todo')
+    res.render('todo', {todos: data})
   })
-  app.post('/todo', (req, res) => {
-
+  app.post('/todo', urlencodedParser, (req, res) => {
+    data.push(req.body)
+    res.json(data)
   })
-  app.post('/todo', (req, res) => {
-
+  app.delete('/todo/:item', (req, res) => {
+    data = data.filter((todo) => {
+      return todo.item.replace(/ /g, "-") !== req.params.item
+    })
+    res.json(data)
   })
 }
