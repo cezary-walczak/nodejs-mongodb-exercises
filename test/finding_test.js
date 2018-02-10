a4433d92fa89e6eedfc21ff1a316e2bdecd978a7
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-02-10 17:32:23 +0100

    object id

--- test/finding_test.js
+++ test/finding_test.js
@@ -3,9 +3,26 @@ const NinjaChar = require('../models/ninjachar')
 
 describe('finding records', () => {
 
+  var char;
+  beforeEach((done) => {
+    char = new NinjaChar({
+      name: 'Shaun'
+    });
+    char.save().then(() => {
+      done();
+    });
+  });
+
   it('find one record from database', (done) => {
     NinjaChar.findOne({ name: 'Shaun' }).then((result) => {
       assert(result.name === 'Shaun')
+      done()
+    })
+  })
+
+  it('find one record by ID from database', (done) => {
+    NinjaChar.findOne({ _id: char._id }).then((result) => {
+      assert(result._id.toString() === char._id.toString())
       done()
     })
   })
