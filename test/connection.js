15b42618fabbfab59ab9378832fcae800628bec6
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-02-10 15:55:54 +0100

    dropping a collection

--- test/connection.js
+++ test/connection.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/tester');
+mongoose.Promise = global.Promise
 
-mongoose.connection.once('open', () => {
-  console.log('Connection has been made, now make fireworks...')
-}).once('error', () => {
-  console.log('Connection error: ', error)
+before((done) => {
+  mongoose.connect('mongodb://localhost/tester');
+
+  mongoose.connection.once('open', () => {
+    console.log('Connection has been made, now make fireworks...')
+    done()
+  }).on('error', () => {
+    console.log('Connection error: ', error)
+  })
+})
+
+beforeEach((done) => {
+  mongoose.connection.collection.ninjachars.drop(() => { done() })
 })
