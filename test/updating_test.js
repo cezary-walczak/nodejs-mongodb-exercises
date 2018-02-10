2849e3759c9dda393ccf5f7fd95ba79d7cc159d5
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-02-10 19:00:13 +0100

    update operators

--- test/updating_test.js
+++ test/updating_test.js
@@ -6,7 +6,8 @@ describe('updating records', () => {
   var char;
   beforeEach((done) => {
     char = new NinjaChar({
-      name: 'Shaun'
+      name: 'Shaun',
+      age: 27
     });
     char.save().then(() => {
       done();
@@ -19,6 +20,15 @@ describe('updating records', () => {
         assert(result.name === 'Yoshi')
         done()
       })
+    })
+  })
+
+  it('increments weights by 1', (done) => {
+    NinjaChar.update({}, { $inc: {weight: 1} }).then(() => {
+      NinjaChar.findOne({ name: 'Shaun' }).then((result) => {
+        assert(result.age === 28)
+        done()
+      });
     })
   })
 
